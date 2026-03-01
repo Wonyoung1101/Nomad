71dce994a34c2b60fb1b5344cad82cfa3d9d98b2
daniela-cruz-3 <daniela3690@icloud.com>
2026-03-01 11:32:04 -0500

    Update api_stubs.java

--- api_stubs.java
+++ api_stubs.java
@@ -1,1 +1,30 @@
 // here we can begin to stub out the API calls that we will be making to the backend
+public class api_stubs{
+//Wonyoung
+
+
+//Seni
+
+
+//Daniela - Reviews and Community
+//Reviews
+public ReviewService{
+    List<Review> listReviews(String targetType, String targetId) {
+        //Retrieves reviews for the sort of business they are trying to find
+    }
+
+    Review submitReview(String userId, String targetType, String targetId, ReviewForm form) {
+        //User is able to find the business they are looking for and submit a review
+    }
+
+    ReviewResponse respondToReview(String userId, String reviewId, String text){
+        //User responds to other reviews
+    }
+}
+
+
+
+//Chris
+
+
+}
