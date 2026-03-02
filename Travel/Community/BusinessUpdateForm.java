82d282b6c9ed9d2d9f24c00d5081b0ebeafe39f3
daniela-cruz-3 <daniela3690@icloud.com>
2026-03-01 20:46:40 -0500

    Update BusinessUpdateForm.java

--- Travel/Community/BusinessUpdateForm.java
+++ Travel/Community/BusinessUpdateForm.java
@@ -1,5 +1,13 @@
 package Travel.Community;
 
 public class BusinessUpdateForm {
-    
+    private String update;
+
+    public BusinessUpdateForm(String update){
+        this.update = update;
+    }
+
+    public String getUpdate() {
+        return update;
+    }
 }
