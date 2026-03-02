8caf49b26a85a1be153f4faa37919bbcdf9fe785
daniela-cruz-3 <daniela3690@icloud.com>
2026-03-01 21:11:38 -0500

    Update SubmissionStatusService.java

--- Travel/Community/SubmissionStatusService.java
+++ Travel/Community/SubmissionStatusService.java
@@ -1,7 +1,12 @@
 package Travel.Community;
 
+import java.util.*;
+
 public class SubmissionStatusService {
+    private Map<String, SubmissionStatus> submissionStatuses = new HashMap<>();
+
     public SubmissionStatus getStatus(String submissionId){
         //business verification (rejection or acceptance)
+        return submissionStatuses.get(submissionId);
     }   
 }
