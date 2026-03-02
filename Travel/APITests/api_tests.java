be7a84f8128a39fe6001e97495a28da14a61dfcf
daniela-cruz-3 <daniela3690@icloud.com>
2026-03-01 22:04:00 -0500

    Update api_tests.java

--- Travel/APITests/api_tests.java
+++ Travel/APITests/api_tests.java
@@ -3,6 +3,8 @@ package Travel.APITests;
 import Travel.Reviews.*;
 import Travel.Community.*;
 
+import java.util.*;
+
 public class api_tests {
     public static void main(String[] args) {
         //where we can test our apis
@@ -23,7 +25,22 @@ public class api_tests {
 
         //Reviews - Daniela 
         System.out.println("Review Tests: ");
+        ReviewService reviewService = new ReviewService();
+        ReviewForm form1 = new ReviewForm("This is a great place to visit!");
+        Review review1 = reviewService.submitReview("user1", "business", "target1", form1);
 
+        if(review1 != null){
+            System.out.println("Review submitted successfully: " + review1.getReviewText());
+        } else {
+            System.out.println("Review submission failed.");
+        }
+
+        List<Review> reviews = reviewService.listReviews("business", "target1");
+        if(!reviews.isEmpty()){
+            System.out.println("Review listed successfully: " + reviews.get(0).getReviewText());
+        } else {
+            System.out.println("No reviews found for target1.");
+        }
 
         //Community - Daniela
         System.out.println("Community Tests: ");
