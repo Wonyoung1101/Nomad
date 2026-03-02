e56b2030f5bf720119a83ff1d77f26a6ec265f20
daniela-cruz-3 <daniela3690@icloud.com>
2026-03-01 21:58:17 -0500

    Update api_tests.java

--- Travel/APITests/api_tests.java
+++ Travel/APITests/api_tests.java
@@ -1,5 +1,37 @@
 package Travel.APITests;
 
+import Travel.Reviews.*;
+import Travel.Community.*;
+
 public class api_tests {
-//where we can test our apis
+    public static void main(String[] args) {
+        //where we can test our apis
+
+        System.out.println("API TESTS: \n");
+
+        //Authentication - Wonyoung
+
+
+        //Accommodation - Wonyoung
+
+
+        //Activities - Seni
+
+
+        //Itinerary - Seni
+
+
+        //Reviews - Daniela 
+        System.out.println("Review Tests: ");
+
+
+        //Community - Daniela
+        System.out.println("Community Tests: ");
+
+
+        //Reporting - Chris
+
+
+        //Administration - Chris
+    }
 }
