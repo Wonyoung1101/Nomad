6c73f69b346ff8d10c41dd1874d95b63a4b23d68
daniela-cruz-3 <daniela3690@icloud.com>
2026-03-01 21:22:14 -0500

    Update ReviewForm.java

--- Travel/Reviews/ReviewForm.java
+++ Travel/Reviews/ReviewForm.java
@@ -1,5 +1,13 @@
 package Travel.Reviews;
 
 public class ReviewForm {
-    
+    private String reviewText;
+
+    public ReviewForm(String reviewText){
+        this.reviewText = reviewText;
+    }
+
+    public String getReviewForm(){
+        return reviewText;
+    }
 }
