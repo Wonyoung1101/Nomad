a17fa55f48f9905f4772b9591b1d0eaa4b41d361
daniela-cruz-3 <daniela3690@icloud.com>
2026-03-01 20:53:36 -0500

    Update SubmissionReceipt.java

--- Travel/Community/SubmissionReceipt.java
+++ Travel/Community/SubmissionReceipt.java
@@ -1,5 +1,13 @@
 package Travel.Community;
 
 public class SubmissionReceipt {
-    
+    private String submissionId;
+
+    public SubmissionReceipt(String submissionId) {
+        this.submissionId = submissionId;
+    }
+
+    public String getSubmissionId(){
+        return submissionId;
+    }
 }
