44945949fae155bcf28cbad0081a15b3cf432153
daniela-cruz-3 <daniela3690@icloud.com>
2026-04-06 16:46:44 -0400

    integreated supabase database into backend

---
+++ Travel/Supabase.java
@@ -0,0 +1,6 @@
+package Travel;
+
+public class Supabase {
+    public static final String SUPABASE_URL = "https://bwclxbaofljoezxpakgm.supabase.co";
+    public static final String SUPABASE_API_KEY = "sb_publishable_kPwVhOLwXshraopkfegmUg_zJutfRcc";
+}
